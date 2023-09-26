98fdd3191204eaeb0a05950ad289fa342e9a51c4
nsousa30 <nsousa@ua.pt>
2023-09-26 10:33:18 +0100

    1

---
+++ Parte02/Ex1/ex1.py
@@ -0,0 +1,25 @@
+import cv2
+import copy
+def main():
+    #Load Image
+    image_original = cv2.imread('Images/lake.jpg')
+
+
+    h,w,nc = image_original.shape
+
+    image = copy.deepcopy(image_original)
+    reductions = [0,10,20,40,50]
+    for reduction in reductions:   
+        for row in range (0,h):
+            for col in range (int(w/2),w):
+                image[row, col,0] = max(image[row,col,0]-reduction,0)
+                image[row, col, 1] = max(image[row,col,1]-reduction,0)
+                image[row, col, 2] = max(image[row,col,2]-reduction,0)
+
+        cv2.imshow('Nightfall',image)
+        cv2.waitKey(0)
+    
+    cv2.waitKey(0)
+
+if __name__ == "__main__":
+    main()
