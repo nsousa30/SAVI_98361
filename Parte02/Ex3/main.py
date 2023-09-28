cec97fb322c4d597e9891a48d1c2625b492d5bd2
nsousa30 <nsousa@ua.pt>
2023-09-28 09:06:24 +0100

    Ex3 completo

--- Parte02/Ex3/main.py
+++ Parte02/Ex3/main.py
@@ -49,6 +49,33 @@ def main():
 
     cv.destroyAllWindows()
 
+    scene = cv.imread('../images/beach.jpg')
+    
+    cv.namedWindow('Scene')
+    cv.setMouseCallback('Scene',on_mouse)
+
+    while True:
+        cv.imshow('Scene', scene)
+        
+        if region_selected:
+            
+            x1,y1,x2,y2 = selected_region
+            template = scene[y1:y2, x1:x2]
+
+            result = cv.matchTemplate(scene,template, cv.TM_CCOEFF_NORMED)
+
+            _, _, _, max_loc = cv.minMaxLoc(result)
+            h,w,_= template.shape
+            cv.rectangle(scene, (max_loc[0],max_loc[1]),(max_loc[0]+w, max_loc[1]+h),(0,255,0),4)
+
+            cv.imshow('Template', template)
+            region_selected = False
+
+
+        key = cv.waitKey(1) & 0xFF
+        if key == 27:
+            break
+
 
 if __name__ == "__main__":
     main()
